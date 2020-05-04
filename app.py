9bd2401ecd0522eda62a3565a70faba248f518dc
Estefany Torres <e.torresamesty@gmail.com>
2020-05-04 15:24:14 -0500

    test

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title = "Test"
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
