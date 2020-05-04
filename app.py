d8605ebe2ce58dd88e4b17f5de7541df2d333730
Estefany Torres <e.torresamesty@gmail.com>
2020-05-04 15:26:12 -0500

    deply

--- app.py
+++ app.py
@@ -1,104 +1,104 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title = "Test"
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
 # import dash
 # import dash_core_components as dcc
 # import dash_html_components as html
-# from dash.dependencies import Input, Output
-# import plotly.graph_objects as go
-# import pandas as pd
 #
-# # Initialize app
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}],)
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # server = app.server
+# app.title = "Test"
 #
-# # Load Data
-# df = pd.read_csv("data/accidents_by_state.csv")
+# app.layout = html.Div(children=[
+#     html.H1(children='Hello Dash'),
 #
-# # Layout
-# app.layout = html.Div(
-#     id="root",
-#     children=[
-#         html.Div(
-#             id="header",
-#             children=[
-#                 html.Img(id="logo", src="https://i.pinimg.com/originals/0d/ce/de/0dcedef0ee93efd1be0e68872f95397c.png"),
-#                 html.H1(children="Traffic Accidents in the U.S."),
+#     html.Div(children='''
+#         Dash: A web application framework for Python.
+#     '''),
+#
+#     dcc.Graph(
+#         id='example-graph',
+#         figure={
+#             'data': [
+#                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+#                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
 #             ],
-#         ),
-#         html.Div(
-#             id="heatmap-container",
-#             children=[
-#                 html.H4("Total accidents per state"),
-#                 dcc.Graph(id="state-choropleth"),
-#                 dcc.Slider(
-#                     id='year-slider',
-#                     min=df['year'].min(),
-#                     max=df['year'].max(),
-#                     value=df['year'].min(),
-#                     marks={str(year): str(year) for year in df['year'].unique()},
-#                     step=None
-#                 )
-#             ],
-#         )
-#     ]
-# )
-#
-#
-# @app.callback(
-#     Output(component_id='state-choropleth', component_property='figure'),
-#     [Input(component_id='year-slider', component_property='value')]
-# )
-# def update_map(year):
-#
-#     temp_df = df[df['year'] == year]
-#     temp_df = temp_df.groupby('state').sum()['count'].reset_index()
-#
-#     fig = go.Figure(data=go.Choropleth(
-#         locations=temp_df['state'],
-#         z=temp_df['count'].astype(float),
-#         locationmode='USA-states',
-#         colorscale='Reds',
-#         colorbar_title="Traffic Accidents",
-#     ))
-#     fig.update_layout(
-#         # title_text='US Traffic accidents',
-#         geo_scope='usa',
+#             'layout': {
+#                 'title': 'Dash Data Visualization'
+#             }
+#         }
 #     )
-#     return fig
-#
+# ])
 #
 # if __name__ == '__main__':
 #     app.run_server(debug=True)
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+import pandas as pd
+
+# Initialize app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}],)
+server = app.server
+
+# Load Data
+df = pd.read_csv("data/accidents_by_state.csv")
+
+# Layout
+app.layout = html.Div(
+    id="root",
+    children=[
+        html.Div(
+            id="header",
+            children=[
+                html.Img(id="logo", src="https://i.pinimg.com/originals/0d/ce/de/0dcedef0ee93efd1be0e68872f95397c.png"),
+                html.H1(children="Traffic Accidents in the U.S."),
+            ],
+        ),
+        html.Div(
+            id="heatmap-container",
+            children=[
+                html.H4("Total accidents per state"),
+                dcc.Graph(id="state-choropleth"),
+                dcc.Slider(
+                    id='year-slider',
+                    min=df['year'].min(),
+                    max=df['year'].max(),
+                    value=df['year'].min(),
+                    marks={str(year): str(year) for year in df['year'].unique()},
+                    step=None
+                )
+            ],
+        )
+    ]
+)
+
+
+@app.callback(
+    Output(component_id='state-choropleth', component_property='figure'),
+    [Input(component_id='year-slider', component_property='value')]
+)
+def update_map(year):
+
+    temp_df = df[df['year'] == year]
+    temp_df = temp_df.groupby('state').sum()['count'].reset_index()
+
+    fig = go.Figure(data=go.Choropleth(
+        locations=temp_df['state'],
+        z=temp_df['count'].astype(float),
+        locationmode='USA-states',
+        colorscale='Reds',
+        colorbar_title="Traffic Accidents",
+    ))
+    fig.update_layout(
+        # title_text='US Traffic accidents',
+        geo_scope='usa',
+    )
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
